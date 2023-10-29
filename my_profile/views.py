9c60a354565e5790197db3348712ce56c2eb1a04
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2023-10-30 00:00:19 +0700

    add change pass and progress literasi

--- my_profile/views.py
+++ my_profile/views.py
@@ -5,6 +5,9 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from my_profile.models import UserProfile, ReadingHistory
 from progress_literasi.models import BukuDibaca    
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.http import JsonResponse
 # Create your views here.
 
 @login_required
@@ -14,8 +17,25 @@ def user_profile(request):
 
     reading_history, created = ReadingHistory.objects.get_or_create(user=user)
 
+    try:
+        user_profile = UserProfile.objects.get(user=request.user)
+        target_buku = user_profile.target_buku
+    except ObjectDoesNotExist:
+        user_profile = None
+        target_buku = 0
+
+    book_count = reading_history.books.count()
+    
+    if target_buku > 0:
+        percentage_complete = (book_count * 100) / target_buku
+    else:
+        percentage_complete = 0
+
     context = {
+        'target_buku': target_buku,
         'reading_history': reading_history,
+        'percentage_complete': percentage_complete,
+        'book_count': book_count,
     }
 
     return render(request, 'profile.html', context)
@@ -32,3 +52,15 @@ def get_reading_history_json(request):
     book_list = [buku_dibaca.buku for buku_dibaca in buku_dibaca_list]
 
     return HttpResponse(serializers.serialize('json', book_list))
+
+@login_required
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            # Perbarui sesi autentikasi pengguna
+            update_session_auth_hash(request, user)
+            return JsonResponse({"success": "Password changed successfully"})
+        else:
+            return JsonResponse({"error": "Invalid password change request"}, status=400)
